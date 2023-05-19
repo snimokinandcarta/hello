c53f2f35b073e46101d51e4d1526bfdeadb8ed55
snimo <sebastian_nimo@hotmail.com>
2023-05-19 13:26:42 -0300

    aaa

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,12 +1,19 @@
 package hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //comentario
 @Controller
 public class GreetingController {
+
+	// Add a Post Controller to return a List of months
+	@PostMapping("/months")
+	public @ResponseBody String months() {
+		return "January, February, March, April, May, June, July, August, September, October, November, December";
+	}
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
